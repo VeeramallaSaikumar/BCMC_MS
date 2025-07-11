87027e7291ee42fd622c30fbd9949c02800bfc6f
Saikumar Veeramalla <saikumar.v@clientservertech.com>
2025-07-11 12:53:41 +0530

    Second Time Add

--- e2e/BGC_MicroServices/Client/Candidate.spec.ts
+++ e2e/BGC_MicroServices/Client/Candidate.spec.ts
@@ -4,17 +4,23 @@ import * as helpers from '../../../utils/helpers';
 test.describe('Add Candidate API @Client', () => {
 
   test('Add_Candidate', async ({ request }) => {
-    const fullname = await helpers.excelfetch('testData/information.xlsx', 'Client', 2, 2);
-    const email = await helpers.excelfetch('testData/information.xlsx', 'Client', 2, 3);
-    const phone = await helpers.excelfetch('testData/information.xlsx', 'Client', 2, 4);
-    const response = await request.post('/candidate/add', {
+    const randomNumber = Math.floor(Math.random() * 1000);
+    const name = await helpers.excelfetch('testData/information.xlsx', 'Client', 2, 2);
+    const fullname = name + ' ' + 'Auto';
+    const email = await helpers.excelfetch('testData/information.xlsx', 'Client', 3, 2);
+    const emailformat=await helpers.excelfetch('testData/information.xlsx', 'Client', 4, 2);
+    const emailID = email + randomNumber + emailformat;
+    const phone = await helpers.excelfetch('testData/information.xlsx', 'Client', 6, 2);
+    const response = await request.post('/user/register/candidate', {
       data: {
-        fullname: fullname,
-        email: email,
-        phone: phone,
+        fullname: name,
+        email: emailID,
+        dateOfBirth: '1990-01-01',
+        mobileNumber: phone,
       },
     });
     const responsebody = await response.json();
+    console.log('Resonse:', responsebody);
     expect(response.status()).toBe(201);
     console.log('\x1b[32m%s\x1b[0m', `Candidate added successfully: ${responsebody.message}`);
   });
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -8,3 +8,10 @@ export function excelfetch(path: string, sheetName: string, rowNumber: number, c
     const cell = worksheet[cellRef];
     return cell ? cell.v : null; // Return cell value or null if cell is empty
 }
+
+export function getTestDataFromExcel(filePath: string, sheetName: string) {
+  const workbook = XLSX.readFile(filePath);
+  const worksheet = workbook.Sheets[sheetName];
+  const testData: any[] = XLSX.utils.sheet_to_json(worksheet);
+  return testData; // Always return all rows as an array
+}
